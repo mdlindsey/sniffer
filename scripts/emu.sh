621d6ed89c04661a6885409652a57c0ed1217bb9
Dan Lindsey <36630328+mdlindsey@users.noreply.github.com>
2022-02-17 15:18:59 -0500

    Dynamic IP (#1)

--- scripts/emu.sh
+++ scripts/emu.sh
@@ -1,6 +1,6 @@
-LOCAL_IP=192.168.0.211      # This will change depending on your current network settings
-DEVICE_ID=Pixel_5           # This will change depending on Android Studio devices/settings
-DEVICE_SIZE="1440x2560"     # You may change this as you please for larger/smaller device window
+LOCAL_IP=$(ipconfig getifaddr en0)  # Hardcode this if ipconfig doesn't return the correct IP
+DEVICE_ID=Pixel_5                   # This will change depending on Android Studio devices/settings
+DEVICE_SIZE="1440x2560"             # You may change this as you please for larger/smaller device window
 ~/Library/Android/sdk/emulator/emulator \
     -avd $DEVICE_ID                     \
     -writable-system                    \
